9c9fa7859d74ea14afd28c39243d45618a4937d7
yuki.yamamoto <yuki.yamamoto0915@gmail.com>
2023-08-23 17:38:34 +0900

    wip

--- main.go
+++ main.go
@@ -2,57 +2,39 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/m4oyu/visualizeChannel/chanx"
+	"os"
+	"strings"
 )
 
 func main() {
-	useChanx()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: mytool <go-file>")
+		return
+	}
+
+	goFilePath := os.Args[1]
+	goFileContent, err := os.ReadFile(goFilePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Inject channel operation into the file content
+	modifiedContent := injectChannelOperation(string(goFileContent))
+
+	modifiedFilePath := "modified_" + goFilePath
+	err = os.WriteFile(modifiedFilePath, []byte(modifiedContent), 0644)
+	if err != nil {
+		fmt.Println("Error writing modified file:", err)
+		return
+	}
+
+	fmt.Println("Modified file written to:", modifiedFilePath)
 }
 
-func useChanx() {
-	// 定義 chan, chan_id
-	// ch1 := make(chan int)
-	// ch2 := make(chan string)
-	ch1 := chanx.Make(1, "ch1")
-	ch2 := chanx.Make(1, "ch2")
+func injectChannelOperation(input string) string {
+	// Inject channel operation here
+	// For example, replace "ch <- value" with "ch <- value + 1"
+	return strings.Replace(input, "ch <- i", "ch <- i + 1", -1)
 
-	// 送信
-	// ch1 <- 100
-	// ch2 <- "hi"
-	ch1.Send(100)
-
-	go func() {
-		ch2.Send("hi")
-	}()
-
-	// 受信
-	// v1 := <-ch1
-	// v2 := <-ch2
-	v1, _ := ch1.Recv()
-	v2, _ := ch2.Recv()
-
-	fmt.Println(v1)
-	fmt.Println(v2)
-
-	ch1.Close()
-	ch2.Close()
-
-}
-
-func wantToDo() {
-	// 定義 chan, chan_id
-	ch1 := make(chan int, 1)
-	ch2 := make(chan string, 1)
-
-	// 送信
-	ch1 <- 100
-	ch2 <- "hi"
-
-	// 受信
-	v1 := <-ch1
-	v2 := <-ch2
-
-	fmt.Println(v1)
-	fmt.Println(v2)
 }
---
+++ test/modified_example.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- i + 1
+			time.Sleep(time.Second)
+		}
+		close(ch)
+	}()
+
+	for value := range ch {
+		fmt.Println("Received:", value)
+	}
+}
