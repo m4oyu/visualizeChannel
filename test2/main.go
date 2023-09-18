38fcd786d6e75194d5693aac67823f7c5ba657c2
yuki.yamamoto <yuki.yamamoto0915@gmail.com>
2023-09-19 01:38:28 +0900

    feat: recv implemented

--- test2/main.go
+++ test2/main.go
@@ -18,7 +18,11 @@ func main() {
 		func main() {
 			ch := make(chan int, 10)
 			ch <- 5
-			fmt.Println(<-ch)
+			recv := <-ch
+			recv := chanx.Recv()
+			<-ch
+			a, b := <-ch1, <-ch2
+			fmt.Println(recv)
 		}
 	`
 
@@ -33,16 +37,40 @@ func main() {
 
 	ast.Inspect(expr, func(n ast.Node) bool {
 
+		if genDecl, ok := n.(*ast.GenDecl); ok && genDecl.Tok == token.IMPORT {
+			fmt.Println("modify import")
+		}
+
+		if funcDecl, ok := n.(*ast.FuncDecl); ok {
+
+			for i := 0; i < len(funcDecl.Body.List); i++ {
+				InspectFunc(i, funcDecl)
+			}
+
+		}
+		return true
+	})
+
+	var buf bytes.Buffer
+	err = format.Node(&buf, fset, expr)
+	if err != nil {
+		fmt.Println("Error generating code:", err)
+		return
+	}
+	fmt.Println(buf.String())
+}
+
+func InspectFunc(index int, funcDecl *ast.FuncDecl) {
+	ast.Inspect(funcDecl.Body.List[index], func(n ast.Node) bool {
+
 		// make
 		if callExpr, ok := n.(*ast.CallExpr); ok {
 			if funIdent, ok := callExpr.Fun.(*ast.Ident); ok && funIdent.Name == "make" {
 				if _, ok := callExpr.Args[0].(*ast.ChanType); ok {
-
 					chanSize := "1"
 					if len(callExpr.Args) > 1 {
 						chanSize = callExpr.Args[1].(*ast.BasicLit).Value
 					}
-
 					newCall := &ast.CallExpr{
 						Fun: &ast.Ident{Name: "chanx.Make"},
 						Args: []ast.Expr{
@@ -56,32 +84,25 @@ func main() {
 		}
 
 		// send
-		if _, ok := n.(*ast.SendStmt); ok {
-
-			// newSend := &ast.CallExpr{
-			// 	Fun: &ast.Ident{
-			// 		Name: "chanx.Send",
-			// 	},
-			// 	Args: []ast.Expr{
-			// 		&ast.BasicLit{Value: sendStmt.Value.(*ast.BasicLit).Value},
-			// 	},
-			// }
-
-			// parentBlock := n.
-			fmt.Printf("%+v\n", n)
+		if sendStmt, ok := n.(*ast.SendStmt); ok {
+			newSend := &ast.ExprStmt{
+				X: &ast.CallExpr{
+					Fun: &ast.Ident{
+						Name: "chanx.Send",
+					},
+					Args: []ast.Expr{
+						&ast.BasicLit{Value: sendStmt.Value.(*ast.BasicLit).Value},
+					},
+				},
+			}
+			funcDecl.Body.List[index] = newSend
 		}
 
 		// recv
+		// if unaryExpr, ok := n.(*ast.UnaryExpr); ok {
+
+		// }
 
 		return true
 	})
-
-	var buf bytes.Buffer
-	err = format.Node(&buf, fset, expr)
-	if err != nil {
-		fmt.Println("Error generating code:", err)
-		return
-	}
-	fmt.Println(buf.String())
-
 }
